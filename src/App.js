1abff676955c8cd02aff0863b3633db6d3c51c13
srusti <srusti.thakkar@edu-test.in>
2021-06-19 10:48:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { withTranslation, Trans } from 'react-i18next'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: "en"
+    }
+  }
+
+  onLanguageHandle = (event) => {
+    let newLang = event.target.value;
+    this.setState({value: newLang})
+    this.props.i18n.changeLanguage(newLang)
+  }
+ 
+  onInputChange = (event) => {
+      console.log(event.target.value)
+  }
+  renderRadioButtons = () => {
+    return (
+      <div ><input
+      checked={this.state.value === 'en'}
+      name="language" onChange={(e) => this.onLanguageHandle(e)} value="en" type="radio" />English &nbsp; 
+      <input name="language" value="jp" 
+      checked={this.state.value === 'jp'}
+      type="radio" onChange={(e) => this.onLanguageHandle(e)} />Gujarati</div>
+    )
+  }
+
+  render () {
+    const {t} = this.props
+    console.log('this is', this)
+    return (
+      <div className="App-header">
+         { this.renderRadioButtons() }
+         <section style={{width: "100%", textAlign: "center", padding: 40}}>
+            <form>
+                <label>{t('author.title1')}</label>
+                <input type="text" style={{marginLeft: 10}}  onChange={(e) => this.onInputChange(e)} ></input><br/>
+                <label>{t('author.title2')}</label>
+                <input type="text" style={{marginLeft: 10}} onChange={(e) => this.onInputChange(e)} ></input><br/>
+                <label>{t('author.password')}</label>
+                <input type="text" style={{marginLeft: 10}} onChange={(e) => this.onInputChange(e)} ></input><br/>
+                <button type="submit">Submit</button>
+            </form>
+         </section>
+        {/* <table>
+          <tbody>
+            <tr>
+              <td style={{width: '20%'}}>{t('author.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('author.value')}</td>
+            </tr>
+            <tr>
+              <td style={{width: '20%'}}>{t('description.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('description.value')}</td>
+            </tr>
+            <tr>
+              <td style={{width: '20%'}}>{t('description.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('description.value')}</td>
+            </tr>
+          </tbody>
+        </table> */}
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+export default withTranslation()(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <I18nextProvider i18n={i18n}>
+       <App />
+    </I18nextProvider>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
